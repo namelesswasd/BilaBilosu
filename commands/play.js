8646b3f8923846cb48f3079f9da7e7c7fba1eba2
namelesswasd <stanescuvladut10@gmail.com>
2021-08-30 01:53:41 +0300

    music 2

--- commands/play.js
+++ commands/play.js
@@ -1,18 +1,30 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
+const {
+    AudioPlayerStatus,
+    StreamType,
+    createAudioPlayer,
+    createAudioResource,
+    joinVoiceChannel,
+} = require('@discordjs/voice')
 
 module.exports = {
     name: 'play',
     description: 'Comanda pentru a reda ceva de pe youtube la bot',
     async execute(message, args){
         const voiceChannel = message.member.voice.channel;
+        const guild = message.guild;
 
         if(!voiceChannel) return message.reply('trebuie sa fii pe un canal tambea.');
         const permissions = voiceChannel.permissionsFor(message.client.user);
         if(!permissions.has("CONNECT") || !permissions.has("SPEAK")) return message.reply('tu nu ai voie boss');
         if(!args.length) return message.reply('ce drq vrei sa iti cant daca nu ai pus nimic?');
 
-        const connection = await voiceChannel.join();
+        const connection = await joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: guild.id,
+            adapterCreator: guild.voiceAdapterCreator,
+        });
 
         const videoFinder = async (query) => {
             const videoResult = await ytSearch(query);
@@ -24,10 +36,19 @@ module.exports = {
 
         if(video){
             const stream = ytdl(video.url, {filter: 'audioonly'});
+            const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+            const player = createAudioPlayer();
+
+            player.play(resource);
+            connection.subscribe(player);
+
+            player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+
+            /*
             connection.play(stream, {seek: 0, volume: 1})
             .on('finish', () =>{
                 voiceChannel.leave();
-            });
+            });*/
 
             await message.reply(`cant **${video.title}**`)
         } else message.reply('nush ce drq mi-ai dat dar nu am gasit');
