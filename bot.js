b61b501097fb3556bfd3b05cbd574af184c5fdc6
namelesswasd <stanescuvladut10@gmail.com>
2021-08-30 13:58:56 +0300

    optimising music

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS)
 
 const bot = new Client({ intents: myIntents });
-const prefix = "//";
+const prefix = "/";
 
 bot.commands = new Collection();
 
