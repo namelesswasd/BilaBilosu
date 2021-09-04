3c9c14e4dd5fd27f9a2ff6ed9a28c4c97754f0e8
namelesswasd <stanescuvladut10@gmail.com>
2021-09-04 14:17:54 +0300

    compatibility with heroku 2

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const {Client, Collection, Intents, MessageEmbed} = require('discord.js');
 
 const fs = require('fs');
-const config = require("./config.json");
+//const config = require("./config.json");
 
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS)
