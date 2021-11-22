436ab084bad55990b1ac646ea274fad4678964f7
namelesswasd <stanescuvladut10@gmail.com>
2021-11-22 23:02:58 +0200

    bulkdelete limit to 100

--- commands/bulkdelete.js
+++ commands/bulkdelete.js
@@ -5,7 +5,7 @@ module.exports = {
         if(message.author.id === "208918353845288960"){
             if(!args[0]){
                 message.channel.send("pune ma un numar wtf.")
-            } else if (args[0] >= 100){
+            } else if (args[0] > 100){
                 message.channel.send("prea mult.")
             } else if (args[0] < 1){
                 message.channel.send("prea putin.")
